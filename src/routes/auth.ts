2fc700409c5649983aad7ab6518342a46f052a2e
stupid-savi <stupid-savi>
2024-11-21 00:33:17 +0530

    add adding try catch for register user

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,17 +3,18 @@ import { AuthController } from '../controllers/AuthController'
 import { UserService } from '../services/UserService'
 import { AppDataSource } from '../config/data-source'
 import { User } from '../entity/User'
+import logger from '../config/logger'
 
 // Note:-  Inversify.js can automate below process
 const userRepository = AppDataSource.getRepository(User)
 const userService = new UserService(userRepository)
-const authController = new AuthController(userService)
+const authController = new AuthController(userService, logger)
 const authRouter = express.Router()
 
 // Can use any of both options for binding of this keyword
 // authRouter.post('/register', authController.register.bind(authController))
-authRouter.post('/register', async (req, res) => {
-  await authController.register(req, res)
+authRouter.post('/register', async (req, res, next) => {
+  await authController.register(req, res, next)
 })
 
 export default authRouter
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,18 +1,27 @@
 import { Repository } from 'typeorm'
 import { User } from '../entity/User'
 import { UserRequestBody } from '../types'
+import createHttpError from 'http-errors'
 
 export class UserService {
-  // eslint-disable-next-line no-unused-vars
-  constructor(private userRepository: Repository<User>) {}
+  constructor(
+    // eslint-disable-next-line no-unused-vars
+    private userRepository: Repository<User>,
+  ) {}
   async create({ firstname, lastname, email, password }: UserRequestBody) {
-    const user = await this.userRepository.save({
-      firstname,
-      lastname,
-      email,
-      password,
-    })
+    try {
+      const user = await this.userRepository.save({
+        firstname,
+        lastname,
+        email,
+        password,
+      })
 
-    return user.id
+      return user.id
+    } catch (err) {
+      console.log(err)
+      const error = createHttpError(500, 'Error creating user in the Database')
+      throw error
+    }
   }
 }
