61255a5fadae98a2e2f195700a120c7de917a05c
stupid-savi <stupid-savi>
2024-11-21 00:33:17 +0530

    add adding try catch for register user

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,30 +1,48 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { UserRequestBody } from '../types'
 import { UserService } from '../services/UserService'
+import { Logger } from 'winston'
 
 export class AuthController {
   userService: UserService
+  logger: Logger
 
-  constructor(userService: UserService) {
+  constructor(userService: UserService, logger: Logger) {
     this.userService = userService
+    this.logger = logger
   }
 
   async register(
     req: Request<unknown, unknown, UserRequestBody>,
     res: Response,
+    next: NextFunction,
   ) {
     const { firstname, lastname, email, password } = req.body
     if (!firstname || !lastname || !email || !password) {
       res.status(400).json({ message: 'Bad Request' })
     }
 
-    const userId = await this.userService.create({
+    this.logger.debug('create user request receive', {
       firstname,
       lastname,
       email,
-      password,
+      password: '*******',
     })
 
-    return res.status(201).json({ id: userId })
+    try {
+      const userId = await this.userService.create({
+        firstname,
+        lastname,
+        email,
+        password,
+      })
+
+      this.logger.info('user created', { id: userId })
+
+      res.status(201).json({ id: userId })
+    } catch (err) {
+      next(err)
+      return
+    }
   }
 }
